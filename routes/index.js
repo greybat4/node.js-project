4861df25e807c5bac5ec618411716ac2f4d2cb61
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-08-22 16:18:02 +0530

    creating partials and layouts

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
 
-router.get('/', homeController.home);
+router.get('/home', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName' , require('./routerfile'));
 
 module.exports = router;
